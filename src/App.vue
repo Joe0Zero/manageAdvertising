1f571d2b04a7b1604ed1453c17d9c249c02f45db
Joseph McMyne <joe@806040.xyz>
2024-07-30 21:39:53 -0400

    chore: Add initial configuration files and dependencies

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<NcAppContent>
+		<div id="advertisingmanager">
+			<h1>Hello world!</h1>
+		</div>
+	</NcAppContent>
+</template>
+
+<script>
+import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
+
+export default {
+	name: 'App',
+	components: {
+		NcAppContent,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+#advertisingmanager {
+	display: flex;
+	justify-content: center;
+	margin: 16px;
+}
+</style>
